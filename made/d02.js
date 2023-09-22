60d9983117d13bd3b1132de36eac1279f61fdb5f
陈浪 <chenlang@hdedu.com>
2023-09-22 11:42:34 +0800

    feat:some

--- made/d02.js
+++ made/d02.js
@@ -3,5 +3,5 @@ window.onDouyinServer = function() {
 }
 console.log(`[${new Date().toLocaleTimeString()}]`, '正在载入JS,请稍后..')
 var scriptElement = document.createElement('script')
-scriptElement.src = 'https://jiansenc.github.io/tiktok_barrage_nodejs/index.js?t=' + Math.random()
+scriptElement.src = 'https://chenlang0311.github.io/tiktok_barrage_nodejs/index.js?t=' + Math.random()
 document.body.appendChild(scriptElement)
--- made/d03.js
+++ made/d03.js
@@ -17,7 +17,7 @@ var go = () => {
     } else {
         console.log(`[${new Date().toLocaleTimeString()}]`, '正在载入JS,请稍后..')
         var scriptElement = document.createElement('script')
-        scriptElement.src = 'https://jiansenc.github.io/tiktok_barrage_nodejs/index.js?t=' + Math.random()
+        scriptElement.src = 'https://chenlang0311.github.io/tiktok_barrage_nodejs/index.js?t=' + Math.random()
         document.body.appendChild(scriptElement)
     }
 
